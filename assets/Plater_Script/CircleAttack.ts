bb476bec1a1f8ac493f8bede5ccb40fd1fbd45e8
Howard <howardm511jb@gmail.com>
2024-06-02 21:57:44 +0800

    player_v2 enemy_v4 UI_v1

--- assets/Plater_Script/CircleAttack.ts
+++ assets/Plater_Script/CircleAttack.ts
@@ -1,16 +1,41 @@
 const { ccclass, property } = cc._decorator;
+import ExperienceSystem from './ExperienceSystem'; 
 
 @ccclass
 export default class CircleAttack extends cc.Component {
     @property
     damage: number = 10;
 
-    @property
-    duration: number = 2;
+    private experienceSystem: ExperienceSystem = null;
+
+    private level: number = 0;
+
 
     onLoad() {
+        this.experienceSystem = cc.find("Canvas/Main Camera/ExperienceBar").getComponent(ExperienceSystem);
+        //console.log(this.experienceSystem);
         this.schedule(this.applyDamage, 1.0);
-        this.scheduleOnce(this.destroySelf, this.duration);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    }
+
+    update(dt){
+        if(this.level == 0){
+            this.damage = 10;
+            this.node.scale = 1;
+        }
+        else if(this.level == 1){
+            this.damage = 15;
+            this.node.scale = 2;
+        }
+        else if(this.level == 2){
+            this.damage = 20;
+            this.node.scale = 3;
+        }
+        else if(this.level == 3){
+            this.damage = 25;
+            this.node.scale = 4;
+        }
+
     }
 
     applyDamage() {
@@ -19,6 +44,18 @@ export default class CircleAttack extends cc.Component {
 
     destroySelf() {
         this.node.destroy();
+    }
+
+    onKeyDown(event: cc.Event.EventKeyboard) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.g:
+                if (this.experienceSystem && this.experienceSystem.upgradePoints>0 && this.level !=3) {
+                    this.experienceSystem.useUpgradePoint();
+                    this.level +=1 ; // 按下 G 键消耗升级点数并增加 CircleAttack
+                    console.log("level :",this.level);
+                }
+                break;
+        }
     }
 }
 
--- assets/Plater_Script/PlayerAttack.ts
+++ assets/Plater_Script/PlayerAttack.ts
@@ -1,4 +1,5 @@
 const { ccclass, property } = cc._decorator;
+import ExperienceSystem from './ExperienceSystem'; 
 
 @ccclass
 export default class PlayerAttack extends cc.Component {
@@ -6,15 +7,26 @@ export default class PlayerAttack extends cc.Component {
     bulletPrefab: cc.Prefab = null;
 
     @property // 射击间隔
-    fireInterval: number = 0.5; // 調整攻擊間隔為 0.5 秒
+    fireInterval: number = 1; // 調整攻擊間隔為 0.5 秒
 
     private fireTimer: number = 0;
+    private level: number = 0;
+    private experienceSystem: ExperienceSystem = null;
+
+    onLoad(){
+        this.experienceSystem = cc.find("Canvas/Main Camera/ExperienceBar").getComponent(ExperienceSystem);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    }
 
     start() {
+        console.log(this.experienceSystem);
         this.fireTimer = this.fireInterval; // 初始化計時器
     }
 
     update(dt: number) {
+        if(this.level == 3){
+            this.fireInterval = 0.5;
+        }
         // 更新計時器
         this.fireTimer += dt;
         // 檢查是否可以發射子彈
@@ -25,12 +37,49 @@ export default class PlayerAttack extends cc.Component {
     }
 
     fireBullets() {
-        if (this.bulletPrefab) {
+        if (this.bulletPrefab && this.level == 0) {
             // 创建四个子弹并射向四个方向
             this.createBullet(cc.v2(0, 1));  // 上
             this.createBullet(cc.v2(0, -1)); // 下
             this.createBullet(cc.v2(1, 0));  // 右
             this.createBullet(cc.v2(-1, 0)); // 左
+        }
+        else if (this.bulletPrefab && this.level == 1) {
+            // 创建四个子弹并射向四个方向
+            this.createBullet(cc.v2(0, 1));  // 上
+            this.createBullet(cc.v2(0, -1)); // 下
+            this.createBullet(cc.v2(1, 0));  // 右
+            this.createBullet(cc.v2(-1, 0)); // 左
+            this.createBullet(cc.v2(1, 1));  // 上
+            this.createBullet(cc.v2(1, -1)); // 下
+            this.createBullet(cc.v2(-1, 1));  // 右
+            this.createBullet(cc.v2(-1, -1)); // 左
+        }
+        else if (this.bulletPrefab && this.level == 2) {
+            // 创建十六个子弹并射向十六个方向
+            const angles = [
+                0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 
+                180, 202.5, 225, 247.5, 270, 292.5, 315, 337.5
+            ];
+            
+            for (let i = 0; i < angles.length; i++) {
+                let radians = angles[i] * (Math.PI / 180);
+                let direction = cc.v2(Math.cos(radians), Math.sin(radians));
+                this.createBullet(direction);
+            }
+        }
+        else if (this.bulletPrefab && this.level == 3) {
+            // 创建四个子弹并射向四个方向
+            const angles = [
+                0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 
+                180, 202.5, 225, 247.5, 270, 292.5, 315, 337.5
+            ];
+            
+            for (let i = 0; i < angles.length; i++) {
+                let radians = angles[i] * (Math.PI / 180);
+                let direction = cc.v2(Math.cos(radians), Math.sin(radians));
+                this.createBullet(direction);
+            }
         }
     }
 
@@ -43,6 +92,19 @@ export default class PlayerAttack extends cc.Component {
         const bulletComponent = bullet.getComponent("Bullet");
         if (bulletComponent) {
             bulletComponent.setDirection(direction);
+        }
+    }
+
+    onKeyDown(event: cc.Event.EventKeyboard) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.f:
+                console.log(this.experienceSystem);
+                if (this.experienceSystem && this.experienceSystem.upgradePoints >0 && this.level !=3) {
+                    this.experienceSystem.useUpgradePoint();
+                    this.level +=1 ; // 按下 G 键消耗升级点数并增加 CircleAttack
+                    console.log("level :",this.level);
+                }
+                break;
         }
     }
 }
